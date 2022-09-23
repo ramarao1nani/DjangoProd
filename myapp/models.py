4769b8127d41d41aed360275a091fa4c85ef1394
ramarao1nani <31947325+ramarao1nani@users.noreply.github.com>
2022-09-23 11:48:35 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from email.mime import image
 from django.db import models
 from django.contrib.auth.models import User
-#  Added a cpmment
+#  Added a comment
 # Create your models here.
 class Product(models.Model):
     def __str__(self):
